7e1441f0f2bcec83fb49f1949c643522db3f6402
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-11-18 15:11:59 -0500

    got all the routes and queries working

--- controllers/plantsController.js
+++ controllers/plantsController.js
@@ -10,11 +10,48 @@ const {
 
 plants.get("/", async (req, res) => {
   const allPlants = await getAllPlants();
-  console.log(`All the plants ${allPlants}`);
   if (allPlants) {
     res.status(200).json(allPlants);
   } else {
     res.status(500).json({ error: "Server Error" });
+  }
+});
+
+plants.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const onePlant = await getOnePlant(id);
+  if (onePlant.id) {
+    res.status(200).json(onePlant);
+  } else {
+    res.status(404).json({ error: "Plant Not Found" });
+  }
+});
+
+plants.post("/", async (req, res) => {
+  const body = req.body;
+  const newPlant = await createPlant(body);
+  res.status(200).json(newPlant);
+});
+
+plants.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedPlant = await deletePlant(id);
+  const allPlants = await getAllPlants();
+  if (deletedPlant.id) {
+    res.status(200).json(allPlants);
+  } else {
+    res.status(404).json({ error: "Plant Not Found" });
+  }
+});
+
+plants.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  const updatedPlant = await updatePlant(id, body);
+  if (updatedPlant.id) {
+    res.status(200).json(updatedPlant);
+  } else {
+    res.status(404).json({ error: "Plant Not Found" });
   }
 });
 
--- queries/plant.js
+++ queries/plant.js
@@ -9,13 +9,67 @@ const getAllPlants = async () => {
   }
 };
 
-const getOnePlant = () => {};
+const getOnePlant = async (id) => {
+  try {
+    const onePlant = await db.one("SELECT * FROM plants WHERE id=$1", id);
+    return onePlant;
+  } catch (error) {
+    return error;
+  }
+};
 
-const createPlant = () => {};
+const createPlant = async (plant) => {
+  try {
+    const newPlant = await db.one(
+      "INSERT INTO plants (name, price, in_stock, moisture_needs, description, safe_for_pets, image_url) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [
+        plant.name,
+        plant.price,
+        plant.in_stock,
+        plant.moisture_needs,
+        plant.description,
+        plant.safe_for_pets,
+        plant.image_url,
+      ]
+    );
+    return newPlant;
+  } catch (error) {
+    return error;
+  }
+};
 
-const deletePlant = () => {};
+const deletePlant = async (id) => {
+  try {
+    const deletedPlant = await db.one(
+      "DELETE FROM plants WHERE id=$1 RETURNING *",
+      id
+    );
+    return deletedPlant;
+  } catch (error) {
+    return error;
+  }
+};
 
-const updatePlant = () => {};
+const updatePlant = async (id, plant) => {
+  try {
+    const updatedPlant = await db.one(
+      "UPDATE plants SET name=$1, price=$2, in_stock=$3, moisture_needs=$4, description=$5, safe_for_pets=$6, image_url=$7 WHERE id=$8 RETURNING *",
+      [
+        plant.name,
+        plant.price,
+        plant.in_stock,
+        plant.moisture_needs,
+        plant.description,
+        plant.safe_for_pets,
+        plant.image_url,
+        id,
+      ]
+    );
+    return updatedPlant;
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports = {
   getAllPlants,
