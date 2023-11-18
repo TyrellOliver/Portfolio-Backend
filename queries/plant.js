37b2831b83b14e9750f12f55fc33833ff409c289
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-11-18 11:09:32 -0500

    created the plant.js to query for the data

---
+++ queries/plant.js
@@ -0,0 +1,19 @@
+const db = require("../db/dbConfig");
+
+const getAllPlants = () => {};
+
+const getOnePlant = () => {};
+
+const createPlant = () => {};
+
+const deletePlant = () => {};
+
+const updatePlant = () => {};
+
+module.exports = {
+  getAllPlants,
+  getOnePlant,
+  createPlant,
+  deletePlant,
+  updatePlant,
+};
